1c1868d4dd1fd87895144bda3c1fe343ce09021b
Teri Pastorino <tpasto@comcast.net>
2015-04-20 15:18:49 -0700

    WIP likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,32 @@
 class LikesController < ApplicationController
+   def create
+     @bookmark = Bookmark.find(params[:bookmark_id])
+     like = current_user.likes.build(bookmark: @bookmark)
+    
+    authorize @like
+    if like.save
+       flash[:notice] = "You successfully liked this"
+       redirect_to [bookmark.topic, @bookmark]
+    else
+      flash[:error] = "There was an error Please try again."
+      redirect_to [bookmark.topic, @bookmark]        
+    end
+   end
+
+   def destroy
+    @bookmark = Bookmark.find(params[:bookmark_id])
+    @like = current_user.likes.find(params[:id])
+
+    authorize @like
+     # Get the bookmark from the params
+     # Find the current user's like with the ID in the params
+ 
+     if like.destroy
+      flash[:notice] = "You successfully unliked this"
+      redirect_to [@bookmark.topic, @bookmark]
+    else
+      flash[:warning] = "There was an error un-likings"
+      redirect_to [@bookmark.topic, @bookmark]
+    end
+  end
 end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,7 @@
 class Bookmark < ActiveRecord::Base
-  #belongs_to :user
-    has_one :user, through: :topic
+  belongs_to :user
+  belongs_to :topic
+    #this line was throwing an error that it could not find association for topic in this model for the bookmark policy
+    #has_one :user, through: :topic
     has_many :likes, dependent: :destroy
 end
