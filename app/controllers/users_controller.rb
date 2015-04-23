9eb7794946ff453121f37de25d84b11a725af00a
Teri Pastorino <tpasto@comcast.net>
2015-04-22 18:51:01 -0700

    add user controller and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,12 @@
 class UsersController < ApplicationController
-  def index
-  end
-
-  def update
-  end
-
   def show
-    @user = User.find(params[:id])
+    @added_bookmarks = current_user.bookmarks
+    @liked_bookmarks = current_user.likes
   end
 
-  def user_params
-    params.require(:user).permit(:name, :likes)
-  end
-
+  def index
+     @users = User.top_rated.paginate(page: params[:page], per_page: 10)
+   end
+  #visible to?
+  #do we need user params?
 end
