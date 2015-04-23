9899d044465b22edd8a11303138d729203d29b6c
Teri Pastorino <tpasto@comcast.net>
2015-04-23 12:45:02 -0700

    embedly commit
    working version of embed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,15 @@ module ApplicationHelper
   require 'embedly'
   require 'json'
 
-  def display(url)
+
+   def display(url)
    embedly_api = Embedly::API.new :key => ENV['EMBEDLY_API_KEY']
    obj = embedly_api.oembed :url => url
-   (obj.first.title).html_safe
+   obj.first
   end
 end
-
+  
+# def display(url)
+  #  embedly_api = Embedly::API.new :key => ENV['EMBEDLY_API_KEY']
+  #  obj = embedly_api.oembed :url => url
+  #  (obj.first.html).html_safe
