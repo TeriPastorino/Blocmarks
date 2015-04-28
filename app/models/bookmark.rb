cf9d12b856cc1edc5a6849399ecfd043726c0fc5
Teri Pastorino <tpasto@comcast.net>
2015-04-28 10:32:56 -0700

    views

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -8,6 +8,8 @@ class Bookmark < ActiveRecord::Base
     #has_one :user, through: :topic
     has_many :likes, dependent: :destroy
 
+  # before_save :set_title
+
   def display
     return @obj if defined?(@obj)
     embedly_api = Embedly::API.new :key => ENV['EMBEDLY_API_KEY']
@@ -15,4 +17,8 @@ class Bookmark < ActiveRecord::Base
     @obj = obj.first
   end
 
+  # set title method that uses the display object to automatically set the title
+  def set_title
+    self.title = display.title
+  end
 end
